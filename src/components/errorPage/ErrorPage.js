dc48765328d15f5c1604601850b9268c5526ce92
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-11-13 20:36:56 +0500

    Create Error Page
    Created ErrorPage.js for error pgae and ErrorPage.css for styling of error page

---
+++ src/components/errorPage/ErrorPage.js
@@ -0,0 +1,12 @@
+// Imports
+// React Imports
+import React from 'react';
+// Styles Imports
+import './ErrorPage.css';
+
+// Error Page
+export const ErrorPage = () => {
+    return (
+            <div className = "errorPage"></div>
+    )
+}
