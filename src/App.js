1a6b602417906067ab9ce129a6f636acb9ae8440
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-11-13 23:50:32 +0500

    Define Routes
    Updated App.js to define Routes for different pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+// Imports
+// React Imports
+// Router Imports
+import { Route, Routes } from 'react-router-dom';
+// Component Imports
+import { Home } from './components/home/Home';
+import { Products } from './components/products/Products';
+import { Gents } from './components/products/Gents';
+import { Ladies } from './components/products/Ladies';
+import { ErrorPage } from './components/errorPage/ErrorPage';
+// Functional Component Imports
+import { GlobalDataProvider } from './functionalComponents/DataProvider';
+// Style Imports
 import './App.css';
 
+// App
 function App() {
   return (
-    <div>
-      Hello world
-    </div>
+    <GlobalDataProvider>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="products" element={<Products/>}/>
+        <Route path="products/gents" element={<Gents></Gents>}></Route>
+        <Route path="products/ladies" element={<Ladies/>}></Route>
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>     
+    </GlobalDataProvider>
   );
 }
-
 export default App;
